67cb04b2d84f695c6a6d7a1ef06c100a0534d24b
Molina-Q <131170327+Molina-Q@users.noreply.github.com>
2023-09-23 21:04:08 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -31,139 +31,133 @@ function togglePricingMenu(pricingDropContentPricingLevel, pricingBoxLevel, idIc
     swapArrowIconById(idIconPricingArrowLevel);
 }
 
+function setColorTheme(themeColor) {
+    rootElement.style.setProperty("--color-primary", `rgb(${themeColor.primary})`); // interpolation 
+    rootElement.style.setProperty("--color-primary-gradient", `rgb(${themeColor.primaryGradient})`);
+    rootElement.style.setProperty("--color-secondary", `rgb(${themeColor.secondary})`);
+    rootElement.style.setProperty("--color-tertiary", `rgb(${themeColor.tertiary})`);
+    // console.log(rootComputedStyle.getPropertyValue("--color-primary"));
+}
+
+/* utilisé pour modifier les valeurs des var du CSS */
+const rootElement = document.querySelector(":root"); 
+
 const themeColors = [
     // blue
     {
         name: "Blue",
-        primary: "rgb(35, 148, 184)",
-        primaryGradient: "rgb(139,195,245)",
-        secondary: "rgb(45, 177, 218)",
-        tertiary: "rgb(0, 108, 138)"
+        primary: "35, 148, 184",
+        primaryGradient: "139, 195, 245",
+        secondary: "45, 177, 218",
+        tertiary: "0, 108, 138"
     },
     // red
     {
         name: "Red",
-        primary: "rgb(184, 35, 35)",
-        primaryGradient: "rgb(245, 139, 139)",
-        secondary: "rgb(224, 42, 42)",
-        tertiary: "rgb(129, 24, 24)"
+        primary: "184, 35, 35",
+        primaryGradient: "245, 139, 139",
+        secondary: "224, 42, 42",
+        tertiary: "129, 24, 24"
     },
     // green
     {
         name: "Green",
-        primary: "rgb(35, 184, 35)",
-        primaryGradient: "rgb(181, 245, 139)",
-        secondary: "rgb(44, 228, 44)",
-        tertiary: "rgb(25, 124, 25)"
+        primary: "35, 184, 35",
+        primaryGradient: "181, 245, 139",
+        secondary: "44, 228, 44",
+        tertiary: "25, 124, 25"
     },
     // purple
     {
         name: "Purple",
-        primary: "rgb(142, 35, 184)",
-        primaryGradient: "rgb(220, 139, 245)",
-        secondary: "rgb(173, 42, 224)",
-        tertiary: "rgb(84, 20, 109)"
+        primary: "142, 35, 184",
+        primaryGradient: "220, 139, 245",
+        secondary: "173, 42, 224",
+        tertiary: "84, 20, 109"
     }
 ];
-
-const rootElement = document.querySelector(":root"); 
-
-function setColorTheme(themeColor) {
-    rootElement.style.setProperty("--color-primary", `${themeColor.primary}`); // interpolation 
-    rootElement.style.setProperty("--color-primary-gradient", `${themeColor.primaryGradient}`);
-    rootElement.style.setProperty("--color-secondary", `${themeColor.secondary}`);
-    rootElement.style.setProperty("--color-tertiary", `${themeColor.tertiary}`);
-    // console.log(rootComputedStyle.getPropertyValue("--color-primary"));
-}
-
-const element = document.getElementById("home");               // <-- id of the button we're transitioning
-let transitionTime = 500 ;          // <-- 100 ms - time our animation will last
-let previousTime, start = 0;        // <-- stores data on animation
-let animationDirection = 'forwards'; // <-- stores the animation direction
-let intervalFrame;                  // <-- stores the interval frame
-let currentPct = 0;                 // <-- current percentage through the animation
-let elapsed = 0;                    // <-- number of frames which have ellapsed 
-
-  
-
 
 themeColors.forEach((themeColor) =>
     document.getElementById("gearTheme" + themeColor.name).addEventListener("click", function(){
 
+    const element = document.querySelector(":root");               // <-- id of the button we're transitioning
+    const transitionTime = 500 ;          // <-- 1000 = 100 ms temps de l'animation
+    let intervalFrame;                  // <-- stores the interval frame
+    let currentPct = 0;                 // <-- current percentage through the animation
+    let elapsed = 0;                    // <-- number of frames which have ellapsed 
+
     const rootComputedStyle = getComputedStyle(rootElement);
 
-    /* me permet de créer un array avec comme valeur couleur en rgb du primary AVANT le changement */
-    let themeLastPrimary= rootComputedStyle.getPropertyValue("--color-primary") ;
-    let themeLastGradient = rootComputedStyle.getPropertyValue("--color-primary-gradient") ;
+    /* array qui stock les valeurs RGB du gradient en string AVANT que setColorTheme soit appelé */
+    const themeLastPrimary= rootComputedStyle.getPropertyValue("--color-primary") ;
+    const themeLastGradient= rootComputedStyle.getPropertyValue("--color-primary-gradient") ;
 
-    let rgbLastPrimary = themeLastPrimary.match(/\d+/g) ;
-    let rgbLastGradient = themeLastGradient.match(/\d+/g) ;
+    /* stock individuellement chaque element du string "r, g, b" crée au dessus dans un array ce qui donne : ["r", "g", "b"] */
+    const rgbLastPrimary = themeLastPrimary.match(/\d+/g) ;
+    const rgbLastGradient = themeLastGradient.match(/\d+/g) ;
 
     const ArrayLastPrimary = [] ; 
-        // if(ArrayLastPrimary != ArrayActualPrimary) {
-        for(let i = 0; i < rgbLastPrimary.length; i++) {
-            ArrayLastPrimary.push(parseInt(rgbLastPrimary[i])) ; 
-        }
-        // }
-
     const ArrayLastGradient = [] ;
-        for(let i = 0; i < rgbLastGradient.length; i++) {
-            ArrayLastGradient.push(parseInt(rgbLastGradient[i])) ;
-        }
+    
+    /* converti en int et stock les valeurs "r", "g" et "b" dans un array */
+    for(let i = 0; i < rgbLastPrimary.length; i++) {
+        ArrayLastPrimary.push(parseInt(rgbLastPrimary[i])) ; 
+    }
+    
+    for(let i = 0; i < rgbLastGradient.length; i++) {
+        ArrayLastGradient.push(parseInt(rgbLastGradient[i])) ;
+    }
 
     setColorTheme(themeColor) ;
 
-    /* me permet de créer un array avec comme valeur la couleur en rgb du primary APRES le changement */
-    let themeActualPrimary = rootComputedStyle.getPropertyValue("--color-primary") ;
-    let themeActualGradient = rootComputedStyle.getPropertyValue("--color-primary-gradient") ;
+    /* array qui stock les valeurs RGB du gradient en string APRES que setColorTheme soit appelé */
+    const themeActualPrimary = themeColor.primary ;
+    const themeActualGradient = themeColor.primaryGradient ;
 
-    let rgbActualPrimary = themeActualPrimary.match(/\d+/g) ;
-    let rgbActualGradient = themeActualGradient.match(/\d+/g) ;
+    /* stock individuellement chaque element du string "r, g, b" crée au dessus dans un array ce qui donne : ["r", "g", "b"]  */
+    const rgbActualPrimary = themeActualPrimary.match(/\d+/g) ;
+    const rgbActualGradient = themeActualGradient.match(/\d+/g) ;
 
     const ArrayActualPrimary = [] ;
-        for(let i = 0; i < rgbActualPrimary.length; i++) {
-            ArrayActualPrimary.push(parseInt(rgbActualPrimary[i])) ;        
-        }
-
     const ArrayActualGradient = [] ;
-        for(let i = 0; i < rgbActualGradient.length; i++) {
-            ArrayActualGradient.push(parseInt(rgbActualGradient[i])) ;
-        }
 
-    console.log("array last puis actual primary :")
-    console.log(ArrayLastPrimary);
-    console.log(ArrayActualPrimary);
+    /* converti en int et stock les valeurs "r", "g" et "b" dans un array */
+    for(let i = 0; i < rgbActualPrimary.length; i++) {
+        ArrayActualPrimary.push(parseInt(rgbActualPrimary[i])) ;        
+    }
 
-    console.log("array last puis actual gradient :")
-    console.log(ArrayLastGradient);
-    console.log(ArrayActualGradient);
-    // Apply our gradient programmatically so we can completely manipulate the gradient from JS rather than CSS
+    for(let i = 0; i < rgbActualGradient.length; i++) {
+        ArrayActualGradient.push(parseInt(rgbActualGradient[i])) ;
+    }
 
-    const gradientStopOne = [ // { r: 35, g: 148, b: 184 } // { r: 184, g: 35, b: 35 }
+    const gradientStopOne = [ 
     { pct: 0,  color: { r: ArrayLastGradient[0], g: ArrayLastGradient[1], b: ArrayLastGradient[2] } }, // couleur de base //  // primaryGradient 
     { pct: 100, color: { r: ArrayActualGradient[0], g: ArrayActualGradient[1], b: ArrayActualGradient[2] } }  // couleur de transition primaire // // primaryGradient
     ]
-    
-    const gradientStopTwo = [ // { r: 139, g: 195, b: 245 } // { r: 245, g: 139, b: 139 }
+
+    const gradientStopTwo = [ 
     { pct: 0,  color: { r: ArrayLastPrimary[0], g: ArrayLastPrimary[1], b: ArrayLastPrimary[2] } }, // couleur de base secondaire // ArrayLastPrimary // primary
     { pct: 100, color: { r: ArrayActualPrimary[0], g: ArrayActualPrimary[1], b: ArrayActualPrimary[2] }}   // couleur de transition secondaire // ArrayActualPrimary // primary
     ];
-    
+
     let c1 = gradientStopOne[0].color ;
     let c2 = gradientStopTwo[0].color ;
 
-    // document.getElementById('home').style.background = `radial-gradient(rgb(${c1.r}, ${c1.g}, ${c1.b}), rgb(${c2.r}, ${c2.g}, ${c2.b}))`;
+    rootElement.style.setProperty("--color-primary", `rgb(${c1.r}, ${c1.g}, ${c1.b})`) ;
+    rootElement.style.setProperty("--color-primary-gradient", `rgb(${c2.r}, ${c2.g}, ${c2.b})`) ;
 
-    // This function transitions between two rgb colors
-    const getColor = function(pct, colorSet) {
-        for (var i = 1; i < colorSet.length - 1; i++) {
+    // This function transitions between two rgb colorsconst getColor = function
+    
+    function colorTransition(pct, colorSet) {
+
+        for (var colorCompteur = 1; colorCompteur < colorSet.length - 1; colorCompteur++) {
             if (pct < colorSet[i].pct) {
                 break;
             }
         }
         // This conversion figures out the transition between two rgb values
-        var lower = colorSet[i - 1];
-        var upper = colorSet[i];
+        var lower = colorSet[colorCompteur - 1];
+        var upper = colorSet[colorCompteur];
         var range = upper.pct - lower.pct;
         var rangePct = (pct - lower.pct) / range;
         var pctLower = 1 - rangePct;
@@ -177,37 +171,25 @@ themeColors.forEach((themeColor) =>
 
         // And returns the rgb code
         return `rgb(${color.r}, ${color.g}, ${color.b})`;
-    }
+    } 
 
-    // This is our animation which we run on hover
-    const animateGradient = function() {
+    function animateGradient() {
         if(intervalFrame === undefined) {
             intervalFrame = setInterval(() => {
                 let time = transitionTime / 1000; // time in seconds
                 let numberOfFrames = time * 60; // 60 frames per second -> 1 second = 60 frames
 
-                // If the animation is going forward
-                if(animationDirection === 'forwards') {
-                    // Add 1 to elapsed
-                    elapsed += 1;
-                    // The elapsed frames out of max frames
-                    currentPct = Math.min(elapsed / numberOfFrames, 1) * 100;
-                } else {
-                    // Otherwise we're going back - subtract 1 from ellapsed
-                    elapsed -= 1;
-                    // The elapsed frames out of max frames
-                    currentPct = Math.max(elapsed / numberOfFrames, 0) * 100;
-                }
+                // Add 1 to elapsed
+                elapsed += 1;
+                // The elapsed frames out of max frames
+                currentPct = Math.min(elapsed / numberOfFrames, 1) * 100;
 
                 // Calculate current color in this time for each gradient color
-                let colorOne = getColor(currentPct, gradientStopOne);
-                let colorTwo = getColor(currentPct, gradientStopTwo);
+                let colorOne = colorTransition(currentPct, gradientStopOne);
+                let colorTwo = colorTransition(currentPct, gradientStopTwo);
 
-                // Generate CSS string
-                let generateGradient = `radial-gradient(${colorOne}, ${colorTwo})`;
-
-                // Add it to our background.
-                document.getElementById(element).style.backgroundImage = generateGradient;
+                element.style.setProperty("--color-primary", `${colorTwo}`);
+                element.style.setProperty("--color-primary-gradient", `${colorOne}`);
 
                 // End the interval when we're done
                 if(currentPct === 100 || currentPct === 0) {
@@ -215,65 +197,15 @@ themeColors.forEach((themeColor) =>
                     intervalFrame = undefined;
                 }
             }, 16.667); // 60 frames per second
-        }
-    };
-
-    // On hover, run our animation
-    document.getElementById("gearTheme" + themeColor.name).addEventListener('click', function() {
-        if(animationDirection == 'backwards' ) {
-            animationDirection = 'forwards'
-            animateGradient();
-        } else {
-        animationDirection = 'backwards'
-        animateGradient();
-        }
-
-    });
-
-    // document.getElementById('gearThemeBlue').addEventListener('click', function() {
-    //     animationDirection = 'backwards'
-    //     animateGradient();
-    // });
+        } 
+    } 
+    animateGradient() ;
     } )
 );   
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-// On hover out, run our animation again, but backwards
-// document.getElementById('featuresDesignIdTrans').addEventListener('mouseleave', function() {
-//     animationDirection = 'backwards';
-//     animateGradient();
-// });
-
-
-// function toggleColorTheme(themeColor) {
-//     const toggleColorThemeNow = document.getElementsByClassName("themeColorChangeBackImg");
-
-//     for (let i = 0; i < toggleColorThemeNow.length; i++) {
-//         const openColorThemeNow = toggleColorThemeNow[i];
-//         openColorThemeNow.classList.toggle(themeColor);
-//     }
-// }
 
 
 /* btn menu burger sur la nav */
 document.getElementById("iconBurger").addEventListener("click", toggleTopnavResponsive);
-
-/* change la couleur en fonction du clique sur les carrés de gear */
-// const mainThemeColor = ["Red", "Green", "Purple"];
-// mainThemeColor.forEach((colorChoice) =>
-//     document.getElementById("gearTheme" + colorChoice).addEventListener("click", () =>
-//         toggleColorTheme("themeIs" + colorChoice)
-//     )
-// );
-
-
 
 const pricingLevels = ["Start", "Adv", "Pro"];
 pricingLevels.forEach((level) =>
